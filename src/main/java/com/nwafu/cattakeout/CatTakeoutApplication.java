d9dd418db44286c4f20173674823ca741dc31af6
SR <610311761@qq.com>
2023-12-04 17:46:04 +0800

    Initial commit

---
+++ src/main/java/com/nwafu/cattakeout/CatTakeoutApplication.java
@@ -0,0 +1,13 @@
+package com.nwafu.cattakeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatTakeoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatTakeoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nwafu/cattakeout/CatTakeoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwafu.cattakeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatTakeoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

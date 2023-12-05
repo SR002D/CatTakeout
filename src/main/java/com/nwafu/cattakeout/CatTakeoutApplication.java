feda48adbdfeda03bcd62273b07aca502adcb2d2
SR <610311761@qq.com>
2023-12-05 13:59:36 +0800

    增加员工登录退出

--- src/main/java/com/nwafu/cattakeout/CatTakeoutApplication.java
+++ src/main/java/com/nwafu/cattakeout/CatTakeoutApplication.java
@@ -1,13 +1,15 @@
 package com.nwafu.cattakeout;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class CatTakeoutApplication {
 
     public static void main(String[] args) {
+        log.info("程序启动成功...");
         SpringApplication.run(CatTakeoutApplication.class, args);
     }
-
 }

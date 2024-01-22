28cca8c8a3bf97c3f49537620818850c0843537c
SR <610311761@qq.com>
2024-01-22 20:44:06 +0800

    更新修复部分bug

--- src/test/java/com/nwafu/cattakeout/CatTakeoutApplicationTests.java
+++ src/test/java/com/nwafu/cattakeout/CatTakeoutApplicationTests.java
@@ -1,16 +1,10 @@
 package com.nwafu.cattakeout;
 
 
-import com.nwafu.cattakeout.common.UserHolder;
-import com.nwafu.cattakeout.pojo.Orders;
-import com.nwafu.cattakeout.service.IOrdersService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Slf4j
 @SpringBootTest
